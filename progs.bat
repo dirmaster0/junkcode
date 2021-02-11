961fc4ed545338accc6acf056b75c655c58c95d5
dirmaster0 <75421251+dirmaster0@users.noreply.github.com>
2021-02-11 04:59:18 -0600

    Create progs.bat

---
+++ progs.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
+start "" "C:\ProgramData\CounterPath\Bria 5\Current\Bria5.exe"
+start "" "C:\Program Files (x86)\Notepad++\notepad++.exe"
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook 2016.lnk"
+start "" "C:\Program Files\Rocket.Chat\Rocket.Chat.exe"
