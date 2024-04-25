f4dc681a9f6445b5928660974c2132ee682b9caf
dirmaster0 <75421251+dirmaster0@users.noreply.github.com>
2024-04-25 11:22:05 -0500

    Update iptables-save-restore-psadrst.sh

--- iptables-save-restore-psadrst.sh
+++ iptables-save-restore-psadrst.sh
@@ -1,14 +1,1 @@
-# Backup IPTables rules to /etc/iptables/rules* files
-sudo iptables-save > /etc/iptables/rules.v4
-sudo ip6tables-save > /etc/iptables/rules.v6
-echo "Save completed!"
-sudo sleep 5
-# Reset PSAD
-sudo psad -R
-sudo sleep 5
-echo "PSAD reset!"
-# Restore IPTables rules from /etc/iptables/rules* files
-sudo iptables-restore < /etc/iptables/rules.v4
-sudo ip6tables-restore < /etc/iptables/rules.v6
-echo "Restore completed!"
- 
+
